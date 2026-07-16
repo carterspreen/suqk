78dd8cefbdefd63d4649d25f13f478fc8e4bfbf4
Carter Spreen <carter01@proton.me>
2026-07-16 03:32:31 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # suqk
-Stochastic Unitary Quantum Krylov using QForte and Qiskit. Project from Frost SURP 2026, California Polytechnic State University, SLO. 
+Stochastic Unitary Quantum Krylov using QForte and Qiskit. Project from Frost SURP 2026, California Polytechnic State University, SLO.
--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "Carter Spreen SUQK on IBM QPU";
 
   inputs = {
+    # Official "nixpkgs" flake for NixOS 26.05
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
 
+    # Dr. Nicholas H. Stair's "qforte" library for Python
     qforte = {
       url = "github:nstair/qforte";
       flake = false;
@@ -20,20 +22,24 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
+      # FHS wrapper needed for conda
       fhs = pkgs.buildFHSEnv {
+        # wrapper filename
         name = "suqk-fhs-wrapper";
+        # wrapper packagelist
         targetPkgs =
           pkgs: with pkgs; [
+            git
+            neovim
             micromamba
           ];
+        # setup conda and install qforte
+        runScript = "${./scripts/setup.sh}";
       };
     in
     {
-      # official formatter for Nix files
+      # formatter for Nix files
       formatter.${system} = pkgs.nixfmt;
-
-      # community formatter for Nix files
-      # formatter.${system} = pkgs.alejandra;
 
       # main development shell for SUQK
       devShells.${system}.default = pkgs.mkShell {
